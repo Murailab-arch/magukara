67d030f6be35af1f44f7317e91ad6b039192d658
sora <sora@wide.ad.jp>
2012-06-13 14:06:16 +0900

    temporary remove netfpga profile

--- boards/netfpga/rtl/setup.v
+++
@@ -1,5 +0,0 @@
-`define	MAGIC_CODE	40'hcc00cc00cc
-`define NETFPGA
-
-`define ENABE_RGMII2
-`define ENABE_RGMII3
--- boards/netfpga/synthesis/load.cmd
+++
@@ -1,6 +0,0 @@
-setMode -bscan
-setCable -p auto
-identify
-assignfile -p 2 -file top.bit
-program -p 2
-quit
